d8e79eec35979e8e4b502111f04c73a03ceac05c
shyamtala003 <shyamtala003@gmail.com>
2023-01-05 23:12:52 +0530

    responsive design added

---
+++ main.js
@@ -0,0 +1,14 @@
+// open close menu
+let menuBtn = document.getElementById('menu_btn');
+let closeBtn = document.getElementById('btn_close');
+let menu = document.getElementById("sidebar");
+console.log(menuBtn);
+menuBtn.addEventListener("click",()=>{
+    menu.classList.add("show_sidebar")
+    console.log(menu.classList);
+})
+
+closeBtn.addEventListener("click",()=>{
+    menu.classList.remove("show_sidebar")
+})
+
